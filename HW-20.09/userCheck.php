4ad62ac59077083e9302383c73dd95c854a3df16
Rostyslav Prorochenko <prorochenko91@gmail.com>
2021-09-17 14:50:43 +0300

    some changes

--- HW-20.09/userCheck.php
+++ HW-20.09/userCheck.php
@@ -20,7 +20,7 @@ while(!feof($file)) {
         echo $getUsername;
         break;
 
-    } elseif ($checkUsername && !$checkPassword ){
+    } elseif (!$checkUsername || !$checkPassword ){
 
         echo 'Не вірний пароль, спробуйте ще!';
         fwrite($logger, date('H:i:s d-m-Y ') . $getUsername .' entered the wrong password!' . PHP_EOL);
