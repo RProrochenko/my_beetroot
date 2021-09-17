52178fbf59999b70c341da9fd750134c19579d5b
Rostyslav Prorochenko <prorochenko91@gmail.com>
2021-09-17 14:47:54 +0300

    some changes

--- HW-20.09/userCheck.php
+++ HW-20.09/userCheck.php
@@ -1,6 +1,5 @@
 <?php
 
-
 $getUsername = $_POST['username'];
 $getPassword = $_POST['password'];
 
@@ -11,14 +10,17 @@ while(!feof($file)) {
     $authParam = explode(' ', trim(fgets($file)));
     $logger = fopen('logs/'. $getUsername . '.log', 'a');
 
-    if ($authParam[0] === $getUsername && $authParam[1] === $getPassword) {
+    $checkLogin = $authParam[0] === $getUsername;
+    $checkPassword = $authParam[1] === $getPassword;
+
+    if ($checkLogin && $checkPassword ) {
 
         fwrite($logger, date('H:i:s d-m-Y ') . $getUsername .' logged in!' . PHP_EOL);
         fclose($logger);
         echo $getUsername;
         break;
 
-    } elseif ($authParam[0] === $getUsername && $authParam[1] !== $getPassword){
+    } elseif ($checkLogin && !$checkPassword ){
 
         echo 'Не вірний пароль, спробуйте ще!';
         fwrite($logger, date('H:i:s d-m-Y ') . $getUsername .' entered the wrong password!' . PHP_EOL);
