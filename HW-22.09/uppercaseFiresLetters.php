dbd299f003e02ccd74af8928efa17122640ff505
Rostyslav Prorochenko <prorochenko91@gmail.com>
2021-09-18 17:31:06 +0300

    add WH-22.09

---
+++ HW-22.09/uppercaseFiresLetters.php
@@ -0,0 +1,5 @@
+<?php
+//- дана строка 'london is the capital of great britain'. Сделайте из нее строку 'London Is The Capital Of Great Britain'
+
+$string = 'london is the capital of great britain' . PHP_EOL;
+echo ucwords($string);
